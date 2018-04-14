fe2caf4d4e8018305a0dfbbc8a53f7c61fcc91bd
JuanGrenz <j.grenz@hotmail.com>
2018-04-14 19:26:24 -0300

    ordenamiento de codigo

--- SuperMenu/supermenu.sh
+++ SuperMenu/supermenu.sh
@@ -28,11 +28,11 @@ imprimir_menu () {
     echo -e "\t\t Opciones:";
     echo "";
     echo -e "\t\t\t a.  Ver estado del proyecto";
-    echo -e "\t\t\t b.  Subir el proyecto al repositorio Git";
-    echo -e "\t\t\t c.  Actualizar proyecto en la PC";
-    echo -e "\t\t\t d.  Verificar aplicaciones instaladas";        
+    echo -e "\t\t\t b.  Subir el proyecto al repositorio Git (PUSH)";
+    echo -e "\t\t\t c.  Actualizar proyecto en la PC (PULL)";
+    echo -e "\t\t\t d.  Verificar si tengo una aplicacion instalada";
     echo -e "\t\t\t e.  Buscar archivos por directorio, extension y nombre"; 
-    echo -e "\t\t\t f.  Busqueda y Concatenado de string"; 
+    echo -e "\t\t\t f.  Busqueda y concatenado de strings";
     echo -e "\t\t\t q.  Salir";
     echo "";
     echo -e "Escriba la opción y presione ENTER";
@@ -49,9 +49,9 @@ imprimir_encabezado () {
     echo -e "`date +"%d-%m-%Y %T" `\t\t\t\t\t USERNAME:$USER";
     echo "";
     #Se agregan colores a encabezado
-    echo -e "\t\t ${bg_blue} ${red} ${bold}--------------------------------------\t${reset}";
+    echo -e "\t\t ${bg_blue} ${red} ${bold}-------------------------------------------------------------\t${reset}";
     echo -e "\t\t ${bold}${bg_blue}${red}$1\t\t${reset}";
-    echo -e "\t\t ${bg_blue}${red} ${bold} --------------------------------------\t${reset}";
+    echo -e "\t\t ${bg_blue} ${red} ${bold}-------------------------------------------------------------\t${reset}";
     echo "";
 }
 
@@ -83,64 +83,60 @@ decidir () {
 # FUNCTIONES del MENU
 #------------------------------------------------------
 a_funcion () {
-        imprimir_encabezado "\tOpción a.  Ver estado del proyecto";
-        decidir "cd $proyectoActual; git status";
+	imprimir_encabezado "\tOpción a.  Ver estado del proyecto";
+	decidir "cd $proyectoActual; git status";
 }
 
+
+
 b_funcion () {
-          imprimir_encabezado "\tOpción b.  Subir el proyecto al repositorio Git (PUSH)";
-#echo 
+	imprimir_encabezado "\tOpción b.  Subir el proyecto al repositorio Git (PUSH)";
 
 read -p "Ingrese el path del archivo a subir: " path
 echo $1;
-    while true; do
-        echo "El path $path es correcto? (s/n)" ;
-            read respuesta;
-            case $respuesta in
-                [Nn]* ) break;;
-                   [Ss]* ) eval $1
+while true;do
+	echo "El path $path es correcto? (s/n)" ;
+	read respuesta;
+	case $respuesta in
+		[Nn]* ) break;;
+		[Ss]* ) eval $1
 		git add -A "$path"
 		echo "El comando ingresado por la terminal fue: git add -A $path "
-		echo 
-                break;;
+	        break;;
 		* ) echo "Por favor tipear S/s ó N/n.";;
 		esac
-		
-    done
-#git add -A "$path"
-#
+	done
+
 read -p "Ingrese un comentario: " comment
 git commit -m  "$comment"
 echo "El comando ingresado por la terminal fue: git commit -m 'comentario' "
 echo $1;
-    while true; do
+while true;do
         echo "Desea subir el archivo? (s/n)";
-            read respuesta;
-            case $respuesta in
-                [Nn]* ) break;;
-                   [Ss]* ) eval $1
+	read respuesta;
+	case $respuesta in
+		[Nn]* ) break;;
+		[Ss]* ) eval $1
 		git push origin master
 		echo "El comando ingresado por la terminal fue: git push origin master"
-		echo Subiendo...
-                break;;
+	        break;;
 		* ) echo "Por favor tipear S/s ó N/n.";;
 		esac
-		
-    done
-     
+	done
 }
+
+
 
 c_funcion () {
-    imprimir_encabezado "\tOpción c.  Actualizar proyecto en la pc (PULL)";        
-
-decidir "cd $proyectoActual; git pull origin master";
+	imprimir_encabezado "\tOpción c.  Actualizar el proyecto en la PC (PULL)";
+	decidir "cd $proyectoActual; git pull origin master";
 }
+
 
 
 d_funcion () {
-	imprimir_encabezado "\tOpción d.  Verificar aplicaciones instaladas";
+	imprimir_encabezado "\tOpción d.  Verificar si tengo una aplicacion instalada";
 
-	#!/bin/bash
 	echo "Introduzca aplicacion a verificar?"
 	read app
 
@@ -148,57 +144,57 @@ d_funcion () {
 	resultado=$( grep -wc "^$app" Copia );
 
 	if [ "$resultado" != "0" ];
-	 then
-		echo "El programa esta instalado"
-	else
-		echo "El programa no esta instalado"
+		then
+			echo "El programa esta instalado"
+		else
+			echo "El programa no esta instalado"
 	fi
-
 }
 
+
+
 e_funcion () {
-    imprimir_encabezado "\tOpción e. Verificar archivos por directorio, extension y nombre";
+    imprimir_encabezado "\tOpción e.  Buscar archivos por directorio, extension y nombre";
 
 read -p "Ingrese path a verificar -> " directorio
-
 read -p "Ingrese el extension del archivo -> " extension
-
 read -p "Ingrese el nombre o parte del nombre del archivo -> " nombre
 
 find $directorio -name "*.$extension" | grep -i "$nombre" > Copia
 
 resultado=$( grep -c "$nombre" Copia );
 if [ "$resultado" != "0" ];
-	 then
+	then
 		echo "Con Directorio: $directorio y Extension $formato existen los siguientes archivos:"
-echo "";
- cat Copia
+		echo "";
+		cat Copia
 	else
-	echo"";
+		echo "";
 		echo "No existen Archivos asociados con el directorio y el extension"
 	fi
 rm Copia
 
 }
 
-f_funcion () {
-	imprimir_encabezado "\tOpción f. Busqueda y Concatenado de string";
-rm out	
-read -p "Ingrese el path completo incluyendo el archivo -> " archivo
 
-read -p "Ingrese un String para buscar en el archivo ->" string
-read -p "Cuantas veces desea concatenar la cadena: " contado
+
+f_funcion () {
+	imprimir_encabezado "\tOpción f. Busqueda y concatenado de strings";
+
+rm salida.out
+
+read -p "Ingrese el path completo incluyendo el archivo -> " archivo
+read -p "Ingrese un String para buscar en el archivo -> " string
+read -p "Cuantas veces desea concatenar la cadena: " limite
 
 contador=1
 
-while [ $contador -le $contado ]; do
-    grep -i  "$string"  $archivo >> out
-    let contador=$contador+1
+while [ $contador -le $limite ];do
+	grep -i  "$string"  $archivo >> salida.out
+	let contador=$contador+1
 done
 
- }
-
-
+}
 
 
 

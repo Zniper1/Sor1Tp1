11f9c975fcef6dab4d7a36fe1f21cacd3ad696b3
JuanGrenz <j.grenz@hotmail.com>
2018-04-24 12:59:52 -0300

    testeando

---
+++ crearAcceso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#---------------------------------------
+
+while  true
+do
+	Name= SuperMenu
+	Comment= Acceso directo Supermenu
+	Exec= $(pwd)/supermenu.sh
+	Icon= $(pwd)/Icono
+	Terminal=true
+	Type=Application
+done
+

6bd778a377afcf75c12c959c49be350fc517e153
JuanGrenz <j.grenz@hotmail.com>
2018-04-24 19:25:18 -0300

    testeo

--- crearAcceso.sh
+++ crearAcceso.sh
@@ -2,7 +2,7 @@
 #---------------------------------------
 while  true
 do
-	touch SuperMenu.desktop
+	touch $(pwd)/SuperMenu.desktop
 	echo "[Desktop Entry]" >> SuperMenu.desktop
 	echo "Name= SuperMenu" >> SuperMenu.desktop
 	echo "Comment= Acceso directo del Supermenu" >> SuperMenu.desktop
@@ -10,7 +10,7 @@ do
 	echo "Icon= $(pwd)/Icono" >> SuperMenu.desktop
 	echo "Terminal=true" >> SuperMenu.desktop
 	echo "Type=Application" >> SuperMenu.desktop
-	chmod 777 SuperMenu.desktop
+	chmod 744 SuperMenu.desktop
 	break;
 done
 
